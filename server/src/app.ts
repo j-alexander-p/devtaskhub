45f517bf12c76c5c991f59725d24fb23347745c8
j-alexander-p <julianpuig940@gmail.com>
2026-07-21 15:07:28 -0700

    added cors middleware to allow frontend requests

--- server/src/app.ts
+++ server/src/app.ts
@@ -7,8 +7,16 @@ import authMiddleware from "./middleware/authMiddleware";
 import { Request, Response } from "express";
 import projectRoutes from "./routes/projects";
 import userRoutes from "./routes/users";
+import cors from "cors";
 
 const app = express();
+
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  }),
+);
 
 //Dependecy Middleware
 app.use(express.json());

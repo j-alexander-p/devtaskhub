c2e419bf6c404cd8ffb13941972a560ce0f51c0b
j-alexander-p <julianpuig940@gmail.com>
2026-07-21 15:08:20 -0700

    added Register page with working form, state, and API call

--- client/src/pages/Register.tsx
+++ client/src/pages/Register.tsx
@@ -10,7 +10,7 @@ function Register() {
   });
   const navigate = useNavigate();
 
-  async function handleSubmit(e: React.FormEvent) {
+  async function handleSubmit(e: React.SubmitEvent<HTMLFormElement>) {
     e.preventDefault();
 
     const response = await fetch("http://localhost:3000/auth/register", {
@@ -28,8 +28,42 @@ function Register() {
     }
   }
 
-  return;
-  <div></div>;
+  function handleChange(e: React.ChangeEvent<HTMLInputElement>) {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        name="username"
+        placeholder="Username"
+        value={formData.username}
+        onChange={handleChange}
+      />
+
+      <input
+        type="email"
+        name="email"
+        placeholder="Email"
+        value={formData.email}
+        onChange={handleChange}
+      />
+
+      <input
+        type="password"
+        name="password"
+        placeholder="Password"
+        value={formData.password}
+        onChange={handleChange}
+      />
+
+      <button type="submit">Register</button>
+    </form>
+  );
 }
 
 export default Register;

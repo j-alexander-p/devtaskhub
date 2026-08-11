857b8fc7d46013fdbb0e4f5f083b85fa87f47f14
j-alexander-p <julianpuig940@gmail.com>
2026-08-11 16:39:13 -0700

    added a ProtectedRoute component for export and wrapping

---
+++ client/src/components/ProtectedRoute.tsx
@@ -0,0 +1,15 @@
+import { Navigate } from "react-router-dom";
+import { useAuth } from "../context/AuthContext";
+import type { ReactNode } from "react";
+
+function ProtectedRoute({ children }: { children: ReactNode }) {
+  const { user } = useAuth();
+
+  if (!user) {
+    return <Navigate to="/login" replace />;
+  } else {
+    return <>{children}</>;
+  }
+}
+
+export default ProtectedRoute;
